16e4ef45eca4c01548fc444ced2422c4259f3ef5
Giovanni Fabbretti <gfabbretti@MBP-di-Giovanni.lan>
2020-04-28 13:08:38 +0200

    u

--- save_features.py
+++ save_features.py
@@ -46,14 +46,16 @@ def save_features(model, data_loader, outfile ):
 
 def get_model(model_name, num_classes=1000):
 
-    resnet152 = models.resnet152(pretrained=False)
+
+    model = models.resnet152(pretrained=False)
+    newmodel = torch.nn.Sequential(*(list(model.children())[:-1]))
 
     model_dict = dict(ResNet10 = ResNetFeat.ResNet10,
                 ResNet18 = ResNetFeat.ResNet18,
                 ResNet34 = ResNetFeat.ResNet34,
                 ResNet50 = ResNetFeat.ResNet50,
                 ResNet101 = ResNetFeat.ResNet101,
-                ResNet152 = resnet152)
+                ResNet152 = newmodel)
 
     return model_dict[model_name]
 
