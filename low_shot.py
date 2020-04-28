c912a4b62d05805fe49172f50105dd2d0cd0c9c7
Giovanni Fabbretti <gfabbretti@MBP-di-Giovanni.lan>
2020-04-28 19:42:25 +0200

    u

--- low_shot.py
+++ low_shot.py
@@ -92,7 +92,7 @@ def training_loop(lowshot_dataset, num_classes, params, batchsize=1000, maxiters
     featdim = lowshot_dataset.featdim()
     model = nn.Linear(featdim, num_classes)
     model = model.cuda()
-    optimizer = torch.optim.SGD(model.parameters(), params.lr, momentum=params.momentum, dampening=params.momentum, weight_decay=params.wd)
+    optimizer = torch.optim.Adam(model.parameters(), params.lr)
 
     loss_function = nn.CrossEntropyLoss()
     loss_function = loss_function.cuda()
