002989edcbad240aa358a99eba16bfd34b1b4cf5
Giovanni Fabbretti <gfabbretti@MBP-di-Giovanni.lan>
2020-04-28 19:14:31 +0200

    u

--- save_features.py
+++ save_features.py
@@ -84,7 +84,7 @@ if __name__ == '__main__':
     checkpoint = torch.load(params.modelfile)
     #strict is necessary because we did transfer learning and we don't have the fc layer
     #in the resnet152 model
-    model.load_state_dict(checkpoint['state_dict'])
+    model.load_state_dict(checkpoint['state_dict'], strict=False)
 
     model.eval()
 
