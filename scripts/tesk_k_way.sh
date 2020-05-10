1933e6d2e5b50058aea457e8a00ea6ebd23156ee
Giovanni Fabbretti <gfabbretti@mac.local>
2020-05-10 23:45:07 +0200

    update

--- scripts/tesk_k_way.sh
+++ scripts/tesk_k_way.sh
@@ -1,9 +1,9 @@
-for i in 1 5 10 15 20 25 30 35 40 45 50 55 60 65 70
+for i in 5 10 15 20 25 30 35 40 45 50 55 60 65 70
 do
   for j in 1 5
   do
     python ./low_shot.py --lowshotmeta label_idx.json \
-      --experimentpath experiment_cfgs/splitfile_1.json \
+      --experimentpath experiment_cfgs/splitfile_{:d}.json \
       --experimentid  $i --lowshotn $j \
       --trainfile features/ResNet152/train.hdf5 \
       --testfile features/ResNet152/val.hdf5 \
@@ -12,7 +12,7 @@ do
       --numclasses 244 \
       --maxiter 10000 \
       --batchsize 64 \
-      --lr 0.1 --wd 0.01 \
+      --lr 0.1 --wd 0.001 \
       --testsetup 1
   done
 done
