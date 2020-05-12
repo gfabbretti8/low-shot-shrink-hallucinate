0834b1eadbf50ff807acb2b7039e05ba450af826
Giovanni Fabbretti <gfabbretti@mac.local>
2020-05-12 11:20:11 +0200

    update

--- scripts/resnet152.sh
+++ scripts/resnet152.sh
@@ -1,7 +1,7 @@
 # Low-shot benchmark without generation
 for i in 1 2 3 4 5
 do
-  for j in 1 2 5 10 20
+  for j in 1 2 5 10
   do
     python ./low_shot.py --lowshotmeta label_idx.json \
       --experimentpath experiment_cfgs/splitfile_{:d}.json \
@@ -9,9 +9,9 @@ do
       --trainfile features/ResNet152/train.hdf5 \
       --testfile features/ResNet152/val.hdf5 \
       --outdir results \
-       --numclasses 244 \
-       --maxiter 50000 \
-       --batchsize 64 \
+      --numclasses 244 \
+      --maxiter 35000 \
+      --batchsize 64 \
       --lr 0.1 --wd 0.01 
   done
 done
